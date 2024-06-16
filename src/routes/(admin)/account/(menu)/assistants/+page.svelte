3b5cb0832f1605846fc86fff3d02d8771d43b34a
mercuryyy <mercuryyy@users.noreply.github.com>
2024-06-15 21:23:47 -0400

    Update +page.svelte

--- src/routes/(admin)/account/(menu)/assistants/+page.svelte
+++ src/routes/(admin)/account/(menu)/assistants/+page.svelte
@@ -204,11 +204,21 @@
         </div>
 
 
-<div role="tablist" class="tabs">
-  <a role="tab" class="tab">Tab 1</a>
-  <a role="tab" class="tab tab-active">Tab 2</a>
-  <a role="tab" class="tab">Tab 3</a>
+
+
+<div role="tablist" class="tabs tabs-lifted">
+  <input type="radio" name="my_tabs_2" role="tab" class="tab" aria-label="Tab 1" />
+  <div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6">Tab content 1</div>
+
+  <input type="radio" name="my_tabs_2" role="tab" class="tab" aria-label="Tab 2" checked />
+  <div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6">Tab content 2</div>
+
+  <input type="radio" name="my_tabs_2" role="tab" class="tab" aria-label="Tab 3" />
+  <div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6">Tab content 3</div>
 </div>
+
+
+
 
         <!-- Tabs -->
         <div class="tabs flex space-x-4 border-b border-gray-600">
